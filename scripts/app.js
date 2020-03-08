d9675a625406ce75a5e730cd67d452920f94e2d5
styles225 <jarrick.anderson@gmail.com>
2020-03-07 21:17:12 -0500

    add slider

---
+++ scripts/app.js
@@ -0,0 +1,41 @@
+const slide = document.querySelector('.carousel-slide');
+const images = document.querySelectorAll('.carousel-slide img');
+
+// buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+// counter
+let counter = 1;
+const size = images[0].clientWidth;
+
+slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+// button listeners
+prevBtn.addEventListener('click', ()=> {
+  if (counter <= 0) return;
+  slide.style.transition = 'transform 0.4s ease-in-out';
+  counter--;
+  slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+nextBtn.addEventListener('click', ()=> {
+  if (counter >= images.length - 1) return;
+  slide.style.transition = 'transform 0.4s ease-in-out';
+  counter++;
+  slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+slide.addEventListener('transitionend', ()=> {
+  if (images[counter].id === 'last-clone') {
+    slide.style.transition = 'none';
+    counter = images.length - 2;
+    slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+
+  if (images[counter].id === 'first-clone') {
+    slide.style.transition = 'none';
+    counter = images.length - counter;
+    slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+})
